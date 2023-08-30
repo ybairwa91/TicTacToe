938b366bfa6a2478f46beacbc7e19808cff810a9
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-08-30 17:53:08 +0530

    ...

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1ButtonElement = document.getElementById("edit-player-1-btn");
+const editPlayer2ButtonElement = document.getElementById("edit-player-2-btn");
+
+function FillThePlayerName() {}
+editPlayer1ButtonElement.addEventListener("click", FillThePlayerName);
