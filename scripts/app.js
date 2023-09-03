13d428bf20d2c3cf3fe2b1bb75b17d6d6874ddff
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-03 18:04:34 +0530

    ....

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,30 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    Symbol: "X",
+  },
+  {
+    name: "",
+    Symbol: "O",
+  },
+];
+
 const playerConfigOverElement = document.getElementById("config-overlay");
 const backDropElement = document.getElementById("backdrop");
 
 const formElement = document.querySelector("form");
 const errorOutputElement = document.getElementById("config-error");
+const gameAreaElement = document.querySelector("#active-game");
+
+// const formControlElement = document.querySelector(".form-control");
 
 const editPlayer1ButtonElement = document.getElementById("edit-player-1-btn");
 const editPlayer2ButtonElement = document.getElementById("edit-player-2-btn");
 
 const cancelConfigElement = document.getElementById("Cancel-config-button");
+const startNewBtnElement = document.getElementById("start-game-btn");
 
 editPlayer1ButtonElement.addEventListener("click", openPlayerConfig);
 editPlayer2ButtonElement.addEventListener("click", openPlayerConfig);
@@ -16,3 +33,5 @@ cancelConfigElement.addEventListener("click", closePlyerConfig);
 backDropElement.addEventListener("click", closePlyerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewBtnElement.addEventListener("click", startNewGame);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
+
   playerConfigOverElement.style.display = "block";
   backDropElement.style.display = "block";
 }
@@ -6,6 +8,10 @@ function openPlayerConfig() {
 function closePlyerConfig() {
   playerConfigOverElement.style.display = "none";
   backDropElement.style.display = "none";
+  // formControlElement.classList.remove("error");
+  formElement.firstElementChild.classList.remove("error");
+  errorOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -14,11 +20,26 @@ function savePlayerConfig(event) {
   const formData = new FormData(event.target);
   const enteredPlayername = formData.get("username").trim();
   console.log(enteredPlayername);
-  console.log(event.target.firstElementChild);
 
   if (!enteredPlayername) {
     errorOutputElement.textContent = "Please Enter a valid name!";
     event.target.firstElementChild.classList.add("error");
     return;
   }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  console.log(updatedPlayerDataElement);
+
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  // if (editedPlayer === 1) {
+  //   players[0].name = enteredPlayername;
+  // } else {
+  //   players[1].name = enteredPlayername;
+  // }
+  //or
+  players[editedPlayer].name = enteredPlayername;
+  closePlyerConfig();
 }
